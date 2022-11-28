d4692f7749473b35a98eee7d46ae4e49c89420a8
fateh.aliverdievv <tu7l7qsfn@code.edu.az>
2022-11-28 23:29:01 +0400

    29novembertaskdone

---
+++ staticandsealedandnullabletask/Models/Student.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticandsealedandnullabletask.Models
+{
+    internal class Student
+    {
+        static public int Count;
+        int _id;
+        string _fullname;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value >0 )
+                { 
+                    _id = Count;
+                    Count++;
+                }
+            }
+        }
+        public string Fullname
+        {
+            get { return _fullname; }
+            set
+            {
+                {
+                    value = value.Trim();
+                    _fullname = value;
+
+                }
+            }
+        }
+        static Student()
+        {
+            Count++;
+        }
+        public Student(string fullname)
+        {
+            Id++;
+            Fullname = fullname;
+        }
+
+        public static  bool CheckFullname(string fullnamE)
+        {
+            fullnamE = fullnamE.Trim();
+            string[] fullnameArray = fullnamE.Split(' ');
+            (bool name, bool surname) answer=(false, false);
+            if (fullnameArray.Length < 3 && fullnameArray.Length >= 1 && Letter(fullnamE) && Digit(fullnamE) == false)
+            {
+                for (int i = 0; i < fullnameArray.Length; i++)
+                {
+                    if (char.IsUpper(fullnameArray[i][0]))
+                    {
+                        if (i == 0)
+                        {
+                            answer.name = true;
+                        }
+                        if (i == 1)
+                        {
+                            answer.surname = true;
+                        }
+
+                    }
+                }
+            }
+            //ad soyaddan elave deyer de goture bilir
+            //if (fullnameArray.Length >= 1 && Letter(fullnamE) && Digit(fullnamE)==false)
+            //{
+            //    for (int i = 0; i < fullnameArray.Length; i++)
+            //    {
+            //        if (char.IsUpper(fullnameArray[i][0]))
+            //        {
+            //            if (i == 0)
+            //            {
+            //                answer.name = true;
+            //            }
+            //            if (i == 1)
+            //            {
+            //                answer.surname=true;
+            //            }
+            //        }
+            //    }
+            //}
+            if (answer.name==true && answer.surname==true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool Letter(string fullname)
+        {
+            bool answerr = false;
+            fullname = fullname.Trim();
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsLetter(fullname[i]))
+                {
+                    answerr = true; 
+                }
+            }
+            return answerr;
+        }
+        public static bool Digit(string fullname)
+        {
+            bool answerr = false;
+            fullname = fullname.Trim();
+            for (int i = 0; i < fullname.Length; i++)
+            {
+                if (char.IsDigit(fullname[i]))
+                {
+                    answerr = true;
+                }
+            }
+            return answerr;
+        }
+
+    }
+}
---
+++ staticandsealedandnullabletask/Program.cs
@@ -0,0 +1,41 @@
+﻿using staticandsealedandnullabletask.Models;
+
+namespace staticandsealedandnullabletask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student("Fateh Aliverdiev");
+            //Student student3 = new Student("fateh Aliverdiev");
+            //Student student2= new Student("Fateh      Aliverdiev");
+            //Student student1 = new Student("Fateh aliverdiev");
+            //Console.WriteLine(student.Fullname);
+            //Console.WriteLine(student1.Fullname);
+            //Console.WriteLine(student2.Fullname);
+            //Console.WriteLine(student3.Fullname);
+            //Console.WriteLine(Student.Count);
+            while (true)
+            {           
+                Console.WriteLine("Enter fullname :");
+                string fullname = Console.ReadLine();                
+                if (Student.CheckFullname(fullname))
+                {
+                    Student student1 = new Student(fullname);
+                    Student student2 = new Student(fullname);
+                    Student student = new Student(fullname);
+
+                    Console.WriteLine(student.Id+ " " +fullname);
+                    Console.WriteLine("Student Added");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong fullname try again!");
+                }
+
+
+            }
+        }
+    }
+}
